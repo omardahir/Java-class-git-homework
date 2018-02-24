d16a505cd99476c7f188574b9f5ca3e73d15d11e
Omar Dahir <omaralidahir@gmail.com>
2018-02-24 14:42:29 -0700

    added an assignment

---
+++ animalInheritance/Dog.java
@@ -0,0 +1,9 @@
+public class Dog extends Animal {
+  @Override
+  public void walk() {
+    System.out.println("The dog is walking");
+  }
+  public void bark() {
+    System.out.println("Bark!");
+  }
+}
---
+++ animalInheritance/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    Dog spot = new Dog();
+    spot.walk();
+    spot.bark();
+  }
+}
